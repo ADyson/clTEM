ab7232ecc538ab8834921c71dee3478635398d73
Jon Peters <j.j.p.peters@warwick.ac.uk>
2014-07-08 14:37:18 +0100

    Removed openCL build options (break CPU), and made multislice run to end.

--- GPU TEM-STEM Simulation/MainWindow.xaml.cs
+++ GPU TEM-STEM Simulation/MainWindow.xaml.cs
@@ -404,7 +404,7 @@ namespace GPUTEMSTEMSimulation
                     mCL.GetNumberSlices(ref NumberOfSlices);
                     // Seperate into setup, loop over slices and final steps to allow for progress reporting.
 
-                    for (int i = 1; i <= NumberOfSlices-20; i++)
+                    for (int i = 1; i <= NumberOfSlices; i++)
                     {
                         timer.Start();
                         mCL.MultisliceStep(i, NumberOfSlices);
